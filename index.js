3b0a16f4aa72de66cf1a9a890ccd048d341331d0
NIRUPAM SUR <nirupamsur9@gmail.com>
2022-02-05 13:47:11 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(()=>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrotation= 30*htime + mtime/2;
+    mrotation= 6*mtime;
+    srotation= 6*stime;
+
+    let h=document.getElementById("hour")
+    let m=document.getElementById("minute")
+    let s=document.getElementById("second")
+    
+    h.style.transform=`rotate(${hrotation}deg)`;
+    m.style.transform=`rotate(${mrotation}deg)`;
+    s.style.transform=`rotate(${srotation}deg)`;
+}, 1000);
